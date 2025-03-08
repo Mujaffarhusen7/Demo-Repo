8cad6a64b4d233292b9dbef1792f717c58f33bef
Invinci <mujaffarhusen05@gmail.com>
2025-03-08 15:41:24 +0300

    comment2

--- src/main/java/newsing/luanchingbrowser.java
+++ src/main/java/newsing/luanchingbrowser.java
@@ -8,12 +8,13 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class luanchingbrowser {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.edge.driver", "C:\\Users\\mujaf\\Edgedriver\\msedgedriver.exe");
 		WebDriver driver = new EdgeDriver();
 		driver.get("https://www.amazon.in");
+		Thread.sleep(30);
 	driver.quit();
 		
 
